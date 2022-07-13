e7d6b99e8a33042d04a1bf124dc1582f1740ced1
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-07-13 17:23:04 -0300

    Add: Method GetPrefix;

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿public class Program
+﻿
+public class Program
 {
+    private static readonly IEnumerable<char> _vowels = new[] { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y' };
+
     public static void Main()
     {
-        Console.WriteLine("Hello, world!");
+        Console.WriteLine(GetPrefix("Tdfgest"));
+        string? text;
+        text = Console.ReadLine();
+    }
+
+    private static string GetTranslation(string? text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return "";
+        return default;
+    }
+
+    private static string GetPrefix(string word)
+    {
+        string prefix = "";
+        foreach (var item in word)
+        {
+            if (_vowels.Any(v => v == item))
+                break;
+            prefix += item;
+        }
+        return prefix;
     }
 }
