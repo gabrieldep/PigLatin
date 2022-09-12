5cffd078fd1de891ee5d5cfe88f14343f8df0f69
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-11 22:13:31 -0300

    Revert "Falta colocar os metodos que estao no program em alguma outra pasta para concluir os testes"
    This reverts commit bead2b730f0c0c010a375bb7331e55b5dc0df737.

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,4 +1,3 @@
-using KisTechTest;
 using Xunit;
 
 namespace Test
@@ -8,57 +7,7 @@ namespace Test
         [Fact]
         public void Test1()
         {
-            string word = "Hey you, you shot 1, but not 2, bullet at my eye!";
-            string translation = "Eyhay youyay, youyay otshay 1, utbay otnay 2, ulletbay atay ymay eyeyay!";
-            Assert.Equal(translation, word);
-        }
 
-        [Fact]
-        public void Test2()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
-        }
-
-        [Fact]
-        public void Test3()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
-        }
-
-        [Fact]
-        public void Test4()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
-        }
-
-        [Fact]
-        public void Test5()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
-        }
-
-        [Fact]
-        public void Test6()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
-        }
-
-        [Fact]
-        public void Test7()
-        {
-            string word = "";
-            string translation = "";
-            Assert.Equal(word, translation);
         }
     }
 }
